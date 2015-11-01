0c5bcad95b524dc75abb1010dd17960693f75d35
Mark Shust <mark@shust.com>
2015-11-01 07:28:13 -0500

    Added dotenv. Added tweet on game start. Updated condition to only send one tweet per event loop.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config({silent: true});
+
 if (! process.env.FIREBASE_AUTH_TOKEN) {
   console.log("You must supply FIREBASE_AUTH_TOKEN to run this script.");
   process.exit(1);
@@ -28,6 +30,7 @@ fbRef.authWithCustomToken(process.env.FIREBASE_AUTH_TOKEN, function(err, res) {
 
     fbRef.on('child_added', function(childSnap) {
       childData[childSnap.key()] = {};
+
       childSnap.ref().on('value', function(snap) {
         var val = snap.val();
         var oldVal = childData[snap.key()];
@@ -35,42 +38,47 @@ fbRef.authWithCustomToken(process.env.FIREBASE_AUTH_TOKEN, function(err, res) {
         if (! val || ! oldVal) return;
 
         var status = "";
+        var scoreStr = val.away_team + ' ' + val.away_score
+          + ', ' + val.home_team + ' ' + val.home_score;
+
         childData[snap.key()] = val;
 
-        if ('away_score' in oldVal
-          && 'home_score' in oldVal
-        ) {
-          if (val.away_score > oldVal.away_score) {
-            var scoreDiff = val.away_score - oldVal.away_score;
-            status = val.away_team + ' just scored ' + scoreDiff + '!\n';
-          } else if (val.home_score > oldVal.home_score) {
-            var scoreDiff = val.home_score - oldVal.home_score;
-            status = val.home_team + ' just scored ' + scoreDiff  + '!\n';
-          }
-
-          if (status) {
-            status = '#NFL LiveUpdate:\n' + status + 'The new score is ' + val.away_team + ' ' + val.away_score + ', ' + val.home_team + ' ' + val.home_score;
-          }
-        }
-
         if ('quarter' in oldVal
-          && oldVal.quarter != "P"
-          && oldVal.quarter != "F"
+          && oldVal.quarter != "F" 
           && val.quarter != oldVal.quarter
-        ) {
+        ) { 
           switch (val.quarter) {
+            case '1': status = 'The ' + val.away_team + ' vs ' + val.home_team
+              + ' game is now underway!'; scoreStr = ''; break;
             case '2': status = 'At the end of 1, the score is '; break;
             case 'H': status = 'At halftime, the score is '; break;
             case '4': status = 'After 3, the score is ' ; break;
             case 'F': status = 'The final score is '; break;
             case 'O': status = 'At the end of regulation, the score is '; break;
+          }   
+
+          if (status) {
+            status = '#NFL LiveUpdate:\n' + status + scoreStr;
+          }   
+        } else if (
+          'away_score' in oldVal
+          && 'home_score' in oldVal
+        ) {
+          if (val.away_score > oldVal.away_score) {
+            var scoreDiff = val.away_score - oldVal.away_score;
+
+            status = val.away_team + ' just scored ' + scoreDiff + '!\n';
+          } else if (val.home_score > oldVal.home_score) {
+            var scoreDiff = val.home_score - oldVal.home_score;
+
+            status = val.home_team + ' just scored ' + scoreDiff  + '!\n';
           }
 
           if (status) {
-            status = '#NFL LiveUpdate:\n' + status + val.away_team + ' ' + val.away_score + ', ' + val.home_team + ' ' + val.home_score;
+            status = '#NFL LiveUpdate:\n' + status + 'The new score is ' + scoreStr;
           }
         }
-        
+
         if (status) {
           twitterClient.post('statuses/update', {status: status}, function(error, tweet){
             if (error) {
